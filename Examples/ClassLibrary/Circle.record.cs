0369d027461dbe87e860f6b834c8b2415fc6ecfc
kzrnm <gengesa@gmail.com>
2022-12-01 02:33:21 +0900

    TargetFrameworks

---
+++ Examples/ClassLibrary/Circle.record.cs
@@ -0,0 +1,8 @@
+﻿namespace ClassLibrary;
+
+public record struct Circle(double R)
+{
+    public double Area => 3.1415926535 * R * R;
+    public double Circumference => 2 * 3.1415926535 * R;
+    public override string ToString() => $"r={R}";
+}
