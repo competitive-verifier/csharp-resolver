1ec8620831093095bdd366ff9efb0c18988bb797
kzrnm <gengesa@gmail.com>
2023-09-14 02:28:33 +0900

    Update libs in examples

--- Examples/VerifyApp/AbstractSolvers.cs
+++ Examples/VerifyApp/AbstractSolvers.cs
@@ -1,5 +1,5 @@
 ﻿namespace VerifyApp;
-internal abstract class AbstractSolver : AbstractSolver2 { }
-internal abstract class AbstractSolver2 : AbstractSolver3 { }
+internal abstract class AbstractSolver : AbstractSolver2<int> { }
+internal abstract class AbstractSolver2<T> : AbstractSolver3 { }
 internal abstract class AbstractSolver3 : AbstractSolver4 { }
 internal abstract class AbstractSolver4 : CompetitiveVerifier.ProblemSolver { }
