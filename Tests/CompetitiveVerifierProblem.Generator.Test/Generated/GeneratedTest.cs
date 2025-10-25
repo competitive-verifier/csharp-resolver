b6c74d79eabac11950a34ea2c6b826f262ff8d45
kzrnm <gengesa@gmail.com>
2025-10-26 02:25:53 +0900

    format

--- Tests/CompetitiveVerifierProblem.Generator.Test/Generated/GeneratedTest.cs
+++ Tests/CompetitiveVerifierProblem.Generator.Test/Generated/GeneratedTest.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace CompetitiveVerifierProblem.Generator.Test.Generated;
 public class GeneratedTest
