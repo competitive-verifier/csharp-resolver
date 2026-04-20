a2d6ebf01a45e2544fd6182f5a07da5f99fa6394
kzrnm <gengesa@gmail.com>
2026-04-21 04:05:31 +0900

    Move files

--- Examples/ClassLibrary/CircleExtension.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ClassLibrary;
-
-public static class CircleExtension
-{
-    public static double Circumference(this double v) => new Circle(v).Circumference;
-    public static double Area(this double v) => new Circle(v).Area;
-}
--- Sources/LocalExamples/ClassLibrary/HelloWorld.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-// competitive-verifier: document_title Hello World!
-namespace ClassLibrary;
-
-public static class HelloWorld
-{
-    public static void Say() => Console.WriteLine("Hello World");
-}
